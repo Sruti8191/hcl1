c3b23e50969766e50a315d884da382c7952b7efa
Sruti8191 <84908542+Sruti8191@users.noreply.github.com>
2021-06-03 13:04:17 +0530

    Update SpringDemo1Application.java

--- src/main/java/com/example/spring/SpringDemo1Application.java
+++ src/main/java/com/example/spring/SpringDemo1Application.java
@@ -2,10 +2,17 @@ package com.example.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class SpringDemo1Application {
+public class SpringDemo1Application extends SpringBootServletInitializer {
 
+	@Override
+protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+
+	return application.sources(SpringBootEmployeeApplication.class);
+}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDemo1Application.class, args);
 	}
